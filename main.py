d39bc4a15d346fefa8032ad5409c29b547a13065
Izzy Hymas <izzyhymas@gmail.com>
2024-04-26 10:48:08 -0600

    Implement Assignment Submit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 
-from models import Course, Discussion, DiscussionEntry
+from models import Course, Discussion, DiscussionEntry, AssignmentGroup, Assignment, AssignmentSubmission
 
 from dotenv import load_dotenv
 import requests
@@ -49,3 +49,45 @@ async def post_discussion(course_id: int, topic_id: int, body: DiscussionEntry):
     response = requests.post(url=f"{base_url}/courses/{course_id}/discussion_topics/{topic_id}/entries", headers=headers, data=body.model_dump())
     r_json = response.json()
     return
+
+@app.get("/courses/{course_id}/assignment_groups")
+async def get_assignment_groups(course_id: int):
+    response = requests.get(url=f"{base_url}/courses/{course_id}/assignment_groups?per_page=100", headers=headers)
+    r_json = response.json()
+    print()
+
+    assignment_groups: list[AssignmentGroup] = []
+    for assignment_group_json in r_json:
+        assignment_group = AssignmentGroup(id=assignment_group_json["id"], name=assignment_group_json["name"])
+        assignment_groups.append(assignment_group)
+        return r_json
+
+    return assignment_groups
+
+@app.get("/courses/{course_id}/assignment_groups/{assignment_group_id}/assignments")
+async def get_assignment(course_id: int, assignment_group_id: str) -> list[Assignment]:
+    response = requests.get(url=f"{base_url}/courses/{course_id}/assignment_groups/{assignment_group_id}/assignments", headers=headers)
+    r_json = response.json()
+    print()
+
+    assignments: list[Assignment] = []
+    for assignment_json in r_json:
+        assignment = Assignment(assignment_group_id=assignment_json["assignment_group_id"], id=assignment_json["id"], name=assignment_json["name"])
+        assignments.append(assignment)
+        print()
+
+    return assignments
+
+import json
+
+@app.post("/courses/{course_id}/assignments/{assignment_id}/submissions")
+async def submit_assignment(course_id: int, assignment_group_id: int, assignment_id: int, submission: AssignmentSubmission):
+    submission_body: dict[str, str] = {
+        "submission[url]": submission.submission_url,
+        "submission[submission_type]": submission.submission_type,
+        "comment[text_comment]": submission.comment,
+    }
+
+    response = requests.post(url=f"{base_url}/courses/{course_id}/assignments/{assignment_id}/submissions", headers=headers, data=submission_body)
+    r_json = response.json()
+    print(r_json)
--- models.py
+++ models.py
@@ -12,3 +12,17 @@ class Discussion(BaseModel):
 
 class DiscussionEntry(BaseModel):
     message: str
+
+class AssignmentGroup(BaseModel):
+    id: int
+    name: str
+
+class Assignment(BaseModel):
+    assignment_group_id: int
+    id: int
+    name: str
+
+class AssignmentSubmission(BaseModel):
+    submission_url: str
+    submission_type: str
+    comment: str
